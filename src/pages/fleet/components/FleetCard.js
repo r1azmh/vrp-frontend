b06e85c8717e5af1fa17ca6c8de9dfe410546b21
Riaz Mahmud <riaz.nstu.10@gmail.com>
2024-04-21 22:37:56 +0300

    Updated FleetCard

--- src/pages/fleet/components/FleetCard.js
+++ src/pages/fleet/components/FleetCard.js
@@ -44,7 +44,7 @@ function FleetCard({handleRemove, index, isEdit}) {
                                    id="number-input" {...register(`vehicles.${index}.name`)}
                                    aria-describedby="helper-text-explanation"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="100" required/>
+                                   placeholder="Vehicle Name" required/>
                         </div>
                         <div className="">
                             <label htmlFor="number-input"
