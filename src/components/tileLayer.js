6cfdf3dc06d18f955dcf0e549b16c8ab79052543
muhammad-samsuddin <samsmusa@outlook.com>
2025-09-07 21:56:11 +0600

    stat

--- src/components/tileLayer.js
+++
@@ -1,6 +0,0 @@
-const tileLayer = {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-}
-
-export default tileLayer;
